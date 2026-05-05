8ba901435c7a564ffcf4b04004f083d64786411e
Ivan <bandgamin719@gmail.com>
2026-05-06 01:19:08 +0300

    make postfix and start queue

--- 3lab/main.cpp
+++ 3lab/main.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
 #include "stack_list.h"
+#include "postfix.h"
 
 int main(){
-    StackList<int> stack1;
-    stack1.push(1);
-    stack1.push(5);
-    stack1.push(1488);
-    std::cout<<stack1.pop()<< " " << stack1.pop()<< " " << stack1.pop() << "\n";
 
-    StackList<std::string> stack2;
-    stack2.push("Ivan");
-    stack2.push("Andrey");
-    stack2.push("Evgen");
-    try
-    {
-        std::cout<<stack2.pop()<<std::endl;
-        std::cout<<stack2.pop()<<std::endl;
-        std::cout<<stack2.pop()<<std::endl;
-        std::cout<<stack2.pop()<<std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
+    char postfix[256];
+    getPostfixFromInfix("( 1 + 2 )*(4+5)-6", postfix);
+    std::cout<<postfix<<"\n";
 
 }
--- 3lab/postfix.h
+++ 3lab/postfix.h
@@ -5,7 +5,7 @@
 
 void checkParanthesesBalance(const char* infix){
     int balance = 0;
-    for(int i = 0; infix[i] != '/0'; ++i){
+    for(int i = 0; infix[i] != '\0'; ++i){
         if(infix[i] == '(') balance++;
         else if(infix[i] == ')') balance--;
         if(balance < 0){
@@ -17,6 +17,17 @@ void checkParanthesesBalance(const char* infix){
     }
 };
 
+int getPriority(char op){
+    switch(op){
+        case '+': return 1;
+        case '-': return 1;
+        case '*': return 2;
+        case '/': return 2;
+        default: return 0;
+    }
+
+}
+
 bool isOperator(char c){
     return c == '+' || c == '-' || c == '*' || c == '/';
 };
@@ -24,17 +35,55 @@ bool isOperator(char c){
 void getPostfixFromInfix(const char* infix, char* postfix){
     StackList<char> operators;
     checkParanthesesBalance(infix);
-    bool previousOperand = false;
+    bool expectOperand = true;
+    int postfixIndex = 0;
     for(int i = 0; infix[i] != '\0'; ++i){
         char c = infix[i];
         if(std::isspace(c)) {
             continue;
         }
-        if(isOperator(c) && !previousOperand){
-            throw std::runtime_error("Error: Operator must go after operand");
+        if(c == '('){
+            if(!expectOperand){
+                throw std::runtime_error("Error: parantheses can't go after operand");
+            }
+            operators.push(c);
+            expectOperand = true;
+        }
+        else if (c == ')'){
+            if (expectOperand){
+                throw std::runtime_error("Error: closing parantheses can't go after operator");
+            }
+            char op = operators.pop();
+            while (op != '('){
+                postfix[postfixIndex++]= op;
+                op = operators.pop();
+            }
+        }
+        else if(isOperator(c)){
+            if(expectOperand){
+                throw std::runtime_error("Error: Operator must go after operand");
+            }
+            expectOperand = true;
+            while (!operators.isEmpty() && operators.top()!= '('
+            && (getPriority(operators.top())>=getPriority(c))){
+                postfix[postfixIndex++]= operators.pop();
+            }
+            operators.push(c);
+        }
+        else{
+            if(!expectOperand){
+                throw std::runtime_error("Error: Operand cannot go after another operand");
+            }
+            postfix[postfixIndex++]= c;
+            expectOperand = false;
         }
         
+        
     }
+    while (!operators.isEmpty()){
+        postfix[postfixIndex++]= operators.pop();
+    }
+    postfix[postfixIndex]= '\0';
 };
 
 #endif
